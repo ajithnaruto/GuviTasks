6ee6f7f7ad2e56122b5d74c25b91a1582a8fe93d
Ajit_Naruto <ajithnaruto@gmail.com>
2018-01-25 17:40:15 +0530

    second commit

---
+++ ODDEVEN.C
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int n;
+clrscr();
+printf("Enter a number to find whether it is odd or even");
+scanf("%d",&n);
+if(n%2==0)
+{
+printf("Even");
+}
+else
+{
+printf("Odd");
+}
+}
