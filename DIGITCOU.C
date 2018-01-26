82341c524a81cc931bf412a2fcc3c0a3de3f68c7
ajithnaruto <ajithnaruto@gmail.com>
2018-01-26 21:29:10 +0530

    Tenth commit

---
+++ DIGITCOU.C
@@ -0,0 +1,14 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int n,count=0;
+printf("Enter the number");
+scanf("%d",&n);
+while(n!=0)
+{
+n=n/10;
+count++;
+}
+printf("Total number of digits = %d",count);
+}
