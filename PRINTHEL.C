a12b768b91dd36f57264f926dc8d928f3a92c174
ajithnaruto <ajithnaruto@gmail.com>
2018-01-26 09:02:03 +0530

    seventh commit

---
+++ PRINTHEL.C
@@ -0,0 +1,13 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int n,i;
+clrscr();
+printf("Enter how many times you want to print hello");
+scanf("%d",&n);
+for(i=1;i<=n;i++)
+{
+printf("HELLO\n");
+}
+}
